778652eb809cbd7528a368dd86b571ca1abdb9e4
fukai99 <141290272+fukai99@users.noreply.github.com>
2023-09-10 11:26:20 +0000

    No6fin

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,17 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    book_comment = current_user.book_comments.new(book_comment_params)
-    book_comment.book_id = book.id
-    book_comment.save
+    @book = Book.find(params[:book_id])
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    @book_comment.save
     
   end
 
   def destroy
-    book_comment = BookComment.find(params[:id])
-    book_comment.destroy
-    
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.find_by(id: params[:id],book_id: params[:book_id])
+    @book_comment.destroy
+    @book_comment = BookComment.new
   end
   
   private
